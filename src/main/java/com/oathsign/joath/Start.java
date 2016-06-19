143cd387a0e3e7b6fdc2a810e3adc8330aae6e29
誓言之印 <hhzgq@163.com>
2016-06-20 01:41:33 +0800

    1233312333

---
+++ src/main/java/com/oathsign/joath/Start.java
@@ -0,0 +1,7 @@
+package com.oathsign.joath;
+
+public class Start {
+    public Integer add(int i, int j) {
+        return i+j;
+    }
+}
--- src/test/java/com/oathsign/joath/StartTest.java
+++ src/test/java/com/oathsign/joath/StartTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class StartTest {
@@ -17,7 +18,14 @@ public class StartTest {
     }
 
     @Test
-    public void testTrue(){
+    public void testAdd() {
+        Start start = new Start();
+        Integer result = start.add(1, 2);
+        assertEquals(new Integer(3), result);
+    }
+
+    @Test
+    public void testTrue() {
         assertTrue(true);
     }
 }
